73d5aa8720d82651f1f1ab6846b479aebc42c7a8
Davi Manoel <63107547+Davi-24k@users.noreply.github.com>
2024-07-27 07:09:49 -0300

    Update TesteGoogle.java

--- TesteGoogle.java
+++ TesteGoogle.java
@@ -1,8 +1,8 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
